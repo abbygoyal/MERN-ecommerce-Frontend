f08be294f848503a2f6e51cc6c70dc8616565e0d
abbygoyal <abhishekgoyal3669@gmail.com>
2023-07-21 18:54:22 +0530

    Connected JWT- Backend Part 6 completion

--- src/features/cart/cartAPI.js
+++ src/features/cart/cartAPI.js
@@ -1,9 +1,9 @@
 export function addToCart(item) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/cart', {
-      method: 'POST',
+    const response = await fetch("http://localhost:8080/cart", {
+      method: "POST",
       body: JSON.stringify(item),
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
@@ -11,10 +11,10 @@ export function addToCart(item) {
   });
 }
 
-export function fetchItemsByUserId(userId) {
+export function fetchItemsByUserId() {
   return new Promise(async (resolve) => {
     //TODO: we will not hard-code server URL here
-    const response = await fetch('http://localhost:8080/cart?user=' + userId);
+    const response = await fetch("http://localhost:8080/cart");
     const data = await response.json();
     resolve({ data });
   });
@@ -22,10 +22,10 @@ export function fetchItemsByUserId(userId) {
 
 export function updateCart(update) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/cart/' + update.id, {
-      method: 'PATCH',
+    const response = await fetch("http://localhost:8080/cart/" + update.id, {
+      method: "PATCH",
       body: JSON.stringify(update),
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
@@ -35,9 +35,9 @@ export function updateCart(update) {
 
 export function deleteItemFromCart(itemId) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/cart/' + itemId, {
-      method: 'DELETE',
-      headers: { 'content-type': 'application/json' },
+    const response = await fetch("http://localhost:8080/cart/" + itemId, {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
     });
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
@@ -45,14 +45,14 @@ export function deleteItemFromCart(itemId) {
   });
 }
 
-export function resetCart(userId) {
+export function resetCart() {
   // get all items of user's cart - and then delete each
   return new Promise(async (resolve) => {
-    const response = await fetchItemsByUserId(userId);
+    const response = await fetchItemsByUserId();
     const items = response.data;
     for (let item of items) {
       await deleteItemFromCart(item.id);
     }
-    resolve({ status: 'success' });
+    resolve({ status: "success" });
   });
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,19 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { addToCart, deleteItemFromCart, fetchItemsByUserId, resetCart, updateCart } from './cartAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  addToCart,
+  deleteItemFromCart,
+  fetchItemsByUserId,
+  resetCart,
+  updateCart,
+} from "./cartAPI";
 
 const initialState = {
-  status: 'idle',
+  status: "idle",
   items: [],
 };
 
 export const addToCartAsync = createAsyncThunk(
-  'cart/addToCart',
+  "cart/addToCart",
   async (item) => {
     const response = await addToCart(item);
     // The value we return becomes the `fulfilled` action payload
@@ -16,16 +22,16 @@ export const addToCartAsync = createAsyncThunk(
 );
 
 export const fetchItemsByUserIdAsync = createAsyncThunk(
-  'cart/fetchItemsByUserId',
-  async (userId) => {
-    const response = await fetchItemsByUserId(userId);
+  "cart/fetchItemsByUserId",
+  async () => {
+    const response = await fetchItemsByUserId();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
 export const updateCartAsync = createAsyncThunk(
-  'cart/updateCart',
+  "cart/updateCart",
   async (update) => {
     const response = await updateCart(update);
     // The value we return becomes the `fulfilled` action payload
@@ -34,7 +40,7 @@ export const updateCartAsync = createAsyncThunk(
 );
 
 export const deleteItemFromCartAsync = createAsyncThunk(
-  'cart/deleteItemFromCart',
+  "cart/deleteItemFromCart",
   async (itemId) => {
     const response = await deleteItemFromCart(itemId);
     // The value we return becomes the `fulfilled` action payload
@@ -42,59 +48,59 @@ export const deleteItemFromCartAsync = createAsyncThunk(
   }
 );
 
-export const resetCartAsync = createAsyncThunk(
-  'cart/resetCart',
-  async (userId) => {
-    const response = await resetCart(userId);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
+export const resetCartAsync = createAsyncThunk("cart/resetCart", async () => {
+  const response = await resetCart();
+  // The value we return becomes the `fulfilled` action payload
+  return response.data;
+});
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
-  reducers: {
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(addToCartAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(addToCartAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.items.push(action.payload);
       })
       .addCase(fetchItemsByUserIdAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchItemsByUserIdAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.items = action.payload;
       })
       .addCase(updateCartAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(updateCartAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
-        const index =  state.items.findIndex(item=>item.id===action.payload.id)
+        state.status = "idle";
+        const index = state.items.findIndex(
+          (item) => item.id === action.payload.id
+        );
         state.items[index] = action.payload;
       })
       .addCase(deleteItemFromCartAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(deleteItemFromCartAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
-        const index =  state.items.findIndex(item=>item.id===action.payload.id)
-        state.items.splice(index,1);
+        state.status = "idle";
+        const index = state.items.findIndex(
+          (item) => item.id === action.payload.id
+        );
+        state.items.splice(index, 1);
       })
       .addCase(resetCartAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(resetCartAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.items = [];
-      })
+      });
   },
 });
 
